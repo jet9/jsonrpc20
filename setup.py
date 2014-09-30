2d852cf5e382494f1cd26e107ddf06ed818038e1
Dmitry Kurbatov <dk@dimcha.ru>
2014-10-01 02:53:02 +0400

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jsonrpc20',
+    version='0.1',
+    packages=['jsonrpc20'],
+    url='https://github.com/jet9/jsonrpc20',
+    license='BSD',
+    author='Dmitry Kurbatov',
+    author_email='dk@dimcha.ru',
+    description='Yet another jsonrpc 2.0 python implementation with wsgi support'
+)
